a7b044d740802f8fe21df366a6e604674bd59eb1
Bruno Oyuko <84352218+oyuko-bruno@users.noreply.github.com>
2023-12-29 14:53:14 +0300

    Update deployment.py

--- moviereviews/deployment.py
+++ moviereviews/deployment.py
@@ -4,8 +4,8 @@ from .settings import BASE_DIR
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['moviereviewapp.azurewebsites.net']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['moviereviewapp.azurewebsites.net']]
 DEBUG = False
 
 # WhiteNoise configuration
